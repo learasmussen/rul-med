4ad0ec508780a76667f5f9dc9374f846853da1ac
fridasophiachristensen <frc004@edu.zealand.dk>
2025-10-30 13:51:49 +0100

    indsat knap og ny underside

--- index.php
+++ index.php
@@ -17,21 +17,25 @@ require "settings/init.php";
     <meta name="copyright" content="Information om copyright">
     
     <link href="css/styles.css" rel="stylesheet" type="text/css">
-    
+
+
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 
 <body>
+
 <img src="images/headerimage.png" alt="headerimage" class="img-fluid">
 
 <!-- Hero -->
 <div class="text-center m-2">
-    <h1 class="mb-3">Frihed på hjul</h1>
+    <h1 class="mb-3 fw-bold">FRIHED PÅ HJUL</h1>
 
-    <p>Vi bevæger os forskelligt men vi fortjener alle adgang til de samme steder.
-        <br> <br>
-        Få overblik over tilgængelighed på steder omkring dig og hjælp andre med dine oplevelser.</p>
-</div>
+<p>Vi bevæger os forskelligt men vi fortjener alle adgang til de samme steder.
+    <br>
+    <br>
+    Få overblik over tilgængelighed på steder omkring dig og hjælp andre med dine oplevelser.</p>
+
+    <a href="places.php" class="btn btn-primary">UNDERSØG</a>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
