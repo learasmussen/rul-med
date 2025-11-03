5a62d1412207fa4da1c3bdea6bc734faff5b6759
lea03 <ler002@edu.zealand.dk>
2025-11-03 11:07:13 +0100

    like side færdig

--- likes.php
+++ likes.php
@@ -126,7 +126,7 @@ require "settings/init.php";
 
 
 <nav>
-
+    <?php include "includes/navFooter.php"?>
 </nav>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
