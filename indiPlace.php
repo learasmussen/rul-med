8e8615b6e1d3f0f2a99bf6039a38b04dc75d4616
Svend <svc001@edu.zealand.dk>
2025-11-04 13:14:19 +0100

    Det virkede ikke hvis det var et number, siden databasen bruger en string.

--- indiPlace.php
+++ indiPlace.php
@@ -25,13 +25,24 @@ function getIconColor($access, $property) {
     }
 
     // If property is truthy
-    if ($access->$property === 1) {
+    if ($access->$property === "1") {
         return "1A9531"; // green
+    } elseif ($access->$property === "0") {
+        return "DF1F11"; // red
     }
-
-    // Anything else (false, 0, etc.)
-    return "DF1F11"; // red
+    return "CACACA";
 }
+
+$doorColor = getIconColor($access, 'hasStepAtEntrance');
+echo $doorColor . "-door ";
+$stairColor = getIconColor($access, 'hasRamp');
+echo $stairColor . "-stair ";
+$elevatorColor = getIconColor($access, 'hasElevator');
+echo $elevatorColor . "-elevator ";
+$toiletColor = getIconColor($access, 'hasAccessibleToilet');
+echo $toiletColor . "-toilet ";
+$parkColor = getIconColor($access, 'hasCloseParking');
+echo $parkColor. "-park ";
 ?>
 
 <!DOCTYPE html>
@@ -92,25 +103,21 @@ function getIconColor($access, $property) {
 
                 <!-- Det her ikoner bliver indsættet -->
                 <svg xmlns="http://www.w3.org/2000/svg" width="35" height="28" viewBox="0 0 44 35" fill="none">
-                    <?php $doorColor = getIconColor($access, 'hasStepAtEntrance'); ?>
                     <path d="M42.9 30.625H37.4V7.75532C37.4 5.89184 35.9198 4.37494 34.1 4.37494H26.4V8.74995H33V35H42.9C43.5078 35 44 34.5105 44 33.9062V31.7187C44 31.1144 43.5078 30.625 42.9 30.625ZM21.4665 0.0689765L8.2665 3.46918C7.28681 3.72143 6.6 4.62787 6.6 5.6683V30.625H1.1C0.49225 30.625 0 31.1144 0 31.7187V33.9062C0 34.5105 0.49225 35 1.1 35H24.2V2.2681C24.2 0.792904 22.8552 -0.289227 21.4665 0.0689765ZM18.15 19.6875C17.2391 19.6875 16.5 18.7079 16.5 17.5C16.5 16.2921 17.2391 15.3125 18.15 15.3125C19.0609 15.3125 19.8 16.2921 19.8 17.5C19.8 18.7079 19.0609 19.6875 18.15 19.6875Z"
                           fill="#<?php echo $doorColor; ?>"/>
                 </svg>
 
                 <svg xmlns="http://www.w3.org/2000/svg" width="34" height="34" viewBox="0 0 34 34" fill="none">
-                    <?php $stairColor = getIconColor($access, 'hasRamp'); ?>
                     <path d="M7.08333 4.25C5.51083 4.25 4.25 5.51083 4.25 7.08333V26.9167C4.25 28.4892 5.51083 29.75 7.08333 29.75H26.9167C28.4892 29.75 29.75 28.4892 29.75 26.9167V7.08333C29.75 5.51083 28.4892 4.25 26.9167 4.25H7.08333ZM19.8333 9.91667H26.9167V12.75H22.6667V17H18.4167V21.25H14.1667V25.5H7.08333V22.6667H11.3333V18.4167H15.5833V14.1667H19.8333V9.91667Z"
                           fill="#<?php echo $stairColor; ?>"/>
                 </svg>
 
                 <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" viewBox="0 0 35 35" fill="none">
-                    <?php $elevatorColor = getIconColor($access, 'hasElevator'); ?>
                     <path d="M27.7083 4.375H7.29167C5.6875 4.375 4.375 5.6875 4.375 7.29167V27.7083C4.375 29.3125 5.6875 30.625 7.29167 30.625H27.7083C29.3125 30.625 30.625 29.3125 30.625 27.7083V7.29167C30.625 5.6875 29.3125 4.375 27.7083 4.375ZM12.3958 8.75C13.4021 8.75 14.2188 9.56667 14.2188 10.5729C14.2188 11.5792 13.4021 12.3958 12.3958 12.3958C11.3896 12.3958 10.5729 11.5792 10.5729 10.5729C10.5729 9.56667 11.3896 8.75 12.3958 8.75ZM16.0417 20.4167H14.5833V26.25H10.2083V20.4167H8.75V16.7708C8.75 15.1667 10.0625 13.8542 11.6667 13.8542H13.125C14.7292 13.8542 16.0417 15.1667 16.0417 16.7708V20.4167ZM22.6042 24.7917L18.9583 18.9583H26.25L22.6042 24.7917ZM18.9583 16.0417L22.6042 10.2083L26.25 16.0417H18.9583Z"
                           fill="#<?php echo $elevatorColor; ?>"/>
                 </svg>
 
                 <svg xmlns="http://www.w3.org/2000/svg" width="34" height="34" viewBox="0 0 34 34" fill="none">
-                    <?php $toiletColor = getIconColor($access, 'hasAccessibleToilet'); ?>
                     <path d="M10.625 9.91667C12.1898 9.91667 13.4583 8.64814 13.4583 7.08333C13.4583 5.51853 12.1898 4.25 10.625 4.25C9.06015 4.25 7.79163 5.51853 7.79163 7.08333C7.79163 8.64814 9.06015 9.91667 10.625 9.91667Z"
                           fill="#<?php echo $toiletColor; ?>" stroke="#<?php echo $toiletColor; ?>" stroke-linecap="round" stroke-linejoin="round"/>
                     <path d="M23.375 9.91667C24.9398 9.91667 26.2083 8.64814 26.2083 7.08333C26.2083 5.51853 24.9398 4.25 23.375 4.25C21.8102 4.25 20.5416 5.51853 20.5416 7.08333C20.5416 8.64814 21.8102 9.91667 23.375 9.91667Z"
@@ -122,7 +129,6 @@ function getIconColor($access, $property) {
                 </svg>
 
                 <svg xmlns="http://www.w3.org/2000/svg" width="29" height="29" viewBox="0 0 35 35" fill="none">
-                    <?php $parkColor = getIconColor($access, 'hasCloseParking'); ?>
                     <path d="M31.25 0H3.75C1.67969 0 0 1.67969 0 3.75V31.25C0 33.3203 1.67969 35 3.75 35H31.25C33.3203 35 35 33.3203 35 31.25V3.75C35 1.67969 33.3203 0 31.25 0ZM18.75 22.5H15V26.25C15 26.9375 14.4375 27.5 13.75 27.5H11.25C10.5625 27.5 10 26.9375 10 26.25V8.75C10 8.0625 10.5625 7.5 11.25 7.5H18.75C22.8828 7.5 26.25 10.8672 26.25 15C26.25 19.1328 22.8828 22.5 18.75 22.5ZM18.75 12.5H15V17.5H18.75C20.125 17.5 21.25 16.375 21.25 15C21.25 13.625 20.125 12.5 18.75 12.5Z"
                           fill="#<?php echo $parkColor ?>"/>
                 </svg>
